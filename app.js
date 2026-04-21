208eb0a99b19165ba924bcda0368a9e8ebe10d25
bradleycastillopaulino <bradleycastillopaulino@gmail.com>
2026-04-20 20:22:14 -0400

    fix puerto duplicado

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ module.exports = app;
 
 // Solo para correr el servidor
 if (require.main === module) {
-    const PORT = 3000;
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Servidor corriendo en puerto ${PORT}`);
     });
 }
